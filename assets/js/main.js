5de6ab19c1e8600ba6d6e022fcebbb2f1f1188bd
Tatiana Mezzano <tmezzano@insis.com.uy>
2025-07-12 22:50:17 -0300

    se agregaron los clientes y las órdenes + estilos

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,6 @@ let navigation= document.querySelector(".navigation");
 let main= document.querySelector(".main");
 
 toggle.onclick = function(){
-	navigation.classList.Toggle("active");
-	main.classList.Toggle("active");	
+	navigation.classList.toggle("active");
+	main.classList.toggle("active");	
 };
